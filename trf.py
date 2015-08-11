50910b886ad640245033efee10a62fa6ff6f54ea
Ross Maddox <rkmaddox@uw.edu>
2015-08-11 09:49:26 -0700

    lots of trf improvements

--- trf.py
+++ trf.py
@@ -5,12 +5,25 @@ Created on Sat Jan 10 14:43:16 2015
 @author: rkmaddox
 """
 
-#import scipy.fftpack as fft
-#from scikits.cuda import fft
-#import pyfftw.interfaces.numpy_fft as fft
-import mne.cuda as fft
 import numpy as np
-#from scikits.cuda.linalg import pinv as pinv_cuda  # need to install cula
+try:  # Try to use CUDA fft
+    from mne.cuda import fft, ifft
+except:
+    from scipy.fftpack import fft, ifft
+try:  # Try to use CUDA pseudoinverse
+    from scikits.cuda import linalg
+    linalg.init()
+    from pycuda import gpuarray
+    use_cuda_pinv = True
+except:
+    #from numpy.linalg import pinv
+    use_cuda_pinv = False
+
+if use_cuda_pinv:
+    def pinv(a, rcond=1e-15):
+        return linalg.pinv(gpuarray.to_gpu(a), rcond).get()
+else:
+    from numpy.linalg import pinv
 
 
 def cross_correlation(x, y):
@@ -23,7 +36,7 @@ def fft_pow2(x, n=None, axis=-1):
     if n < x.shape[axis]:
         ValueError('n must not be less than the length of the data.')
     n = 2 ** int(np.ceil(np.log2(n)))
-    return fft.fft(x, n, axis)
+    return fft(x, n, axis)
 
 
 def make_xxt(ac):
@@ -63,13 +76,13 @@ def trf_corr(x_in, x_out, fs, t_start, t_stop, x_in_freq=False):
             raise ValueError("If x_in is in frequency domain, it must be "
                              "longer than x_out.")
         x_in_fft = x_in
-        x_out_fft = fft.fft(x_out, len_sig)
+        x_out_fft = fft(x_out, len_sig)
 
     # compute the autocorrelations
     ac = np.zeros((n_ch_in, n_ch_in, len_trf * 2 - 1))
     for ch0 in range(n_ch_in):
         for ch1 in np.arange(ch0, n_ch_in, dtype=int):
-            ac_temp = np.real(fft.ifft(x_in_fft[ch0] * np.conj(x_in_fft[ch1])))
+            ac_temp = np.real(ifft(x_in_fft[ch0] * np.conj(x_in_fft[ch1])))
             ac[ch0, ch1] = np.append(ac_temp[-len_trf + 1:], ac_temp[:len_trf])
             if ch0 != ch1:
                 ac[ch1, ch0] = ac[ch0, ch1][::-1]
@@ -78,7 +91,7 @@ def trf_corr(x_in, x_out, fs, t_start, t_stop, x_in_freq=False):
     cc = np.zeros((n_ch_out, n_ch_in, len_trf))
     for ch_in in range(n_ch_in):
         for ch_out in range(n_ch_out):
-            cc_temp = np.real(fft.ifft(x_out_fft[ch_out] *
+            cc_temp = np.real(ifft(x_out_fft[ch_out] *
                               np.conj(x_in_fft[ch_in])))
             if trf_start_ind < 0 and trf_stop_ind + 1 > 0:
                 cc[ch_out, ch_in] = np.append(cc_temp[trf_start_ind:],
@@ -108,7 +121,7 @@ def trf_reg(xxt, xy, n_ch_in, lam=0, reg_type='ridge'):
     else:
         ValueError("reg_type must be either 'ridge' or 'laplacian'")
 
-    w = np.dot(np.linalg.pinv(xxt + lam * reg), xy.T).T
+    w = np.dot(pinv(xxt + lam * reg), xy.T).T
     w = w.reshape([n_ch_out, n_ch_in, len_trf])
 
     return w
